c4ceb357aad2e57b0f7c37199181a4de6c7cbf8d
Guillaume Giamarchi <guillaume.giamarchi@gmail.com>
2014-10-20 22:13:13 +0200

    Wait for firewall deletion
    Necessary to be sure that the firewall is
    actually deleted when trying to delete the
    associated policy

--- builtin/providers/openstack/resource_openstack_firewall.go
+++ builtin/providers/openstack/resource_openstack_firewall.go
@@ -1,9 +1,13 @@
 package openstack
 
 import (
+	"errors"
+	"fmt"
 	"log"
+	"time"
 
 	"github.com/haklop/gophercloud-extensions/network"
+	"github.com/hashicorp/terraform/helper/resource"
 	"github.com/hashicorp/terraform/helper/schema"
 	"github.com/racker/perigee"
 	"github.com/rackspace/gophercloud"
@@ -110,5 +114,45 @@ func resourceFirewallDelete(d *schema.ResourceData, meta interface{}) error {
 		return err
 	}
 
-	return networksApi.DeleteFirewall(d.Id())
+	err = networksApi.DeleteFirewall(d.Id())
+
+	if err != nil {
+		return err
+	}
+
+	stateConf := &resource.StateChangeConf{
+		Pending:    []string{"DELETING"},
+		Target:     "DELETED",
+		Refresh:    WaitForFirewallDeletion(networksApi, d.Id()),
+		Timeout:    2 * time.Minute,
+		Delay:      0,
+		MinTimeout: 2 * time.Second,
+	}
+
+	_, err = stateConf.WaitForState()
+
+	return err
+}
+
+func WaitForFirewallDeletion(api network.NetworkProvider, id string) resource.StateRefreshFunc {
+
+	return func() (interface{}, string, error) {
+		fw, err := api.GetFirewall(id)
+		log.Printf("[DEBUG] Get firewall %s => %#v", id, fw)
+
+		if err != nil {
+			httpStatus := err.(*perigee.UnexpectedResponseCodeError)
+			log.Printf("[DEBUG] Get firewall %s status is %d", id, httpStatus.Actual)
+
+			if httpStatus.Actual == 404 {
+				log.Printf("[DEBUG] Firewall %s is actually deleted", id)
+				return "", "DELETED", nil
+			}
+
+			return nil, "", errors.New(fmt.Sprintf("Unexpected status code %d", httpStatus.Actual))
+		}
+
+		log.Printf("[DEBUG] Firewall %s deletion is pending", id)
+		return fw, "DELETING", nil
+	}
 }
