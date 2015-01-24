7c3f95c1472d9f9cdaf44b8f8114e3b895b4fe63
Eric Bellemon <eric.bellemon@gmail.com>
2015-01-24 17:47:53 +0100

    Handle subnet

--- builtin/providers/openstack/resource_openstack_subnet.go
+++ builtin/providers/openstack/resource_openstack_subnet.go
@@ -3,9 +3,9 @@ package openstack
 import (
 	"log"
 
-	"github.com/haklop/gophercloud-extensions/network"
 	"github.com/hashicorp/terraform/helper/schema"
 	"github.com/racker/perigee"
+	"github.com/rackspace/gophercloud/openstack/networking/v2/subnets"
 )
 
 func resourceSubnet() *schema.Resource {
@@ -23,18 +23,22 @@ func resourceSubnet() *schema.Resource {
 			"cidr": &schema.Schema{
 				Type:     schema.TypeString,
 				Required: true,
-			},
-			"ip_version": &schema.Schema{
-				Type:     schema.TypeInt,
-				Required: true,
-			},
-			"enable_dhcp": &schema.Schema{
-				Type:     schema.TypeBool,
-				Required: true,
+				ForceNew: true,
 			},
 			"network_id": &schema.Schema{
 				Type:     schema.TypeString,
 				Required: true,
+				ForceNew: true,
+			},
+			"ip_version": &schema.Schema{
+				Type:     schema.TypeInt,
+				Required: true,
+				ForceNew: true,
+			},
+			"enable_dhcp": &schema.Schema{
+				Type:     schema.TypeBool,
+				Optional: true,
+				Default:  true,
 			},
 		},
 	}
@@ -42,53 +46,56 @@ func resourceSubnet() *schema.Resource {
 
 func resourceSubnetCreate(d *schema.ResourceData, meta interface{}) error {
 	p := meta.(*Config)
-	networksApi, err := p.getNetworkApi()
+	client, err := p.getNetworkClient()
 	if err != nil {
 		return err
 	}
 
-	newSubnet := network.NewSubnet{
-		NetworkId:  d.Get("network_id").(string),
+	enableDHCP := d.Get("enable_dhcp").(bool)
+
+	opts := subnets.CreateOpts{
+		NetworkID:  d.Get("network_id").(string),
 		Name:       d.Get("name").(string),
-		Cidr:       d.Get("cidr").(string),
-		EnableDhcp: d.Get("enable_dhcp").(bool),
+		CIDR:       d.Get("cidr").(string),
+		EnableDHCP: &enableDHCP,
 		IPVersion:  d.Get("ip_version").(int),
 	}
 
-	log.Printf("[DEBUG] Creating subnet: %#v", newSubnet)
+	log.Printf("[DEBUG] Creating subnet: %#v", opts)
 
-	createdSubnet, err := networksApi.CreateSubnet(newSubnet)
+	createdSubnet, err := subnets.Create(client, opts).Extract()
 	if err != nil {
 		return err
 	}
 
-	d.SetId(createdSubnet.Id)
+	d.SetId(createdSubnet.ID)
 
 	return nil
 }
 
 func resourceSubnetDelete(d *schema.ResourceData, meta interface{}) error {
 	p := meta.(*Config)
-	networksApi, err := p.getNetworkApi()
+	client, err := p.getNetworkClient()
 	if err != nil {
 		return err
 	}
 
 	log.Printf("[DEBUG] Destroy subnet: %s", d.Id())
 
-	return networksApi.DeleteSubnet(d.Id())
+	res := subnets.Delete(client, d.Id())
+	return res.Err
 }
 
 func resourceSubnetRead(d *schema.ResourceData, meta interface{}) error {
 	p := meta.(*Config)
-	networksApi, err := p.getNetworkApi()
+	client, err := p.getNetworkClient()
 	if err != nil {
 		return err
 	}
 
 	log.Printf("[DEBUG] Retrieve information about subnet: %s", d.Id())
 
-	subnet, err := networksApi.GetSubnet(d.Id())
+	subnet, err := subnets.Get(client, d.Id()).Extract()
 	if err != nil {
 		httpError, ok := err.(*perigee.UnexpectedResponseCodeError)
 		if !ok {
@@ -104,28 +111,34 @@ func resourceSubnetRead(d *schema.ResourceData, meta interface{}) error {
 	}
 
 	d.Set("name", subnet.Name)
-	d.Set("enable_dhcp", subnet.EnableDhcp)
+	d.Set("cidr", subnet.CIDR)
+	d.Set("enable_dhcp", subnet.EnableDHCP)
+	d.Set("ip_version", subnet.IPVersion)
+	d.Set("network_id", subnet.NetworkID)
 
 	return nil
 }
 
 func resourceSubnetUpdate(d *schema.ResourceData, meta interface{}) error {
 	p := meta.(*Config)
-	networksApi, err := p.getNetworkApi()
+	client, err := p.getNetworkClient()
 	if err != nil {
 		return err
 	}
 
-	desiredSubnet := network.UpdatedSubnet{}
+	opts := subnets.UpdateOpts{}
 
 	if d.HasChange("name") {
-		desiredSubnet.Name = d.Get("name").(string)
+		opts.Name = d.Get("name").(string)
 	}
 
 	if d.HasChange("enable_dhcp") {
-		desiredSubnet.EnableDhcp = d.Get("enable_dhcp").(bool)
+		enableDHCP := d.Get("enable_dhcp").(bool)
+		opts.EnableDHCP = &enableDHCP
 	}
 
-	_, err = networksApi.UpdateSubnet(d.Id(), desiredSubnet)
+	log.Printf("[DEBUG] Updating subnet: %#v", opts)
+
+	_, err = subnets.Update(client, d.Id(), opts).Extract()
 	return err
 }
