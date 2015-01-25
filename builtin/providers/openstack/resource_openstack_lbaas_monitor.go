01965c90b92193eb89bbdd9d376f6de38ef78e35
Eric Bellemon <eric.bellemon@gmail.com>
2015-01-25 15:31:05 +0100

    Add openstack_lbass_monitor resource

---
+++ builtin/providers/openstack/resource_openstack_lbaas_monitor.go
@@ -0,0 +1,202 @@
+package openstack
+
+import (
+	"github.com/hashicorp/terraform/helper/schema"
+	"github.com/racker/perigee"
+	"github.com/rackspace/gophercloud/openstack/networking/v2/extensions/lbaas/monitors"
+)
+
+func resourceLBaaSMonitor() *schema.Resource {
+	return &schema.Resource{
+		Create: resourceLBaaSMonitorCreate,
+		Read:   resourceLBaaSMonitorRead,
+		Update: resourceLBaaSMonitorUpdate,
+		Delete: resourceLBaaSMonitorDelete,
+
+		Schema: map[string]*schema.Schema{
+			"type": &schema.Schema{
+				Type:     schema.TypeString,
+				Required: true,
+				ForceNew: true,
+			},
+			"delay": &schema.Schema{
+				Type:     schema.TypeInt,
+				Required: true,
+			},
+			"timeout": &schema.Schema{
+				Type:     schema.TypeInt,
+				Required: true,
+			},
+			"max_retries": &schema.Schema{
+				Type:     schema.TypeInt,
+				Required: true,
+			},
+			"expected_codes": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"http_method": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"url_path": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"admin_state_up": &schema.Schema{
+				Type:     schema.TypeBool,
+				Optional: true,
+				Default:  true,
+			},
+		},
+	}
+}
+
+func resourceLBaaSMonitorCreate(d *schema.ResourceData, meta interface{}) error {
+
+	p := meta.(*Config)
+	client, err := p.getNetworkClient()
+	if err != nil {
+		return err
+	}
+
+	adminStateUp := d.Get("admin_state_up").(bool)
+	opts := monitors.CreateOpts{
+		Type:         d.Get("type").(string),
+		Delay:        d.Get("delay").(int),
+		Timeout:      d.Get("timeout").(int),
+		MaxRetries:   d.Get("max_retries").(int),
+		AdminStateUp: &adminStateUp,
+	}
+
+	expectedCodes := d.Get("expected_codes").(string)
+	if len(expectedCodes) > 0 {
+		opts.ExpectedCodes = expectedCodes
+	}
+
+	httpMethod := d.Get("http_method").(string)
+	if len(httpMethod) > 0 {
+		opts.HTTPMethod = httpMethod
+	}
+
+	urlPath := d.Get("url_path").(string)
+	if len(urlPath) > 0 {
+		opts.URLPath = urlPath
+	}
+
+	monitor, err := monitors.Create(client, opts).Extract()
+	if err != nil {
+		return err
+	}
+
+	d.SetId(monitor.ID)
+	readMonitor(monitor, d)
+
+	return nil
+
+}
+
+func resourceLBaaSMonitorDelete(d *schema.ResourceData, meta interface{}) error {
+	p := meta.(*Config)
+	client, err := p.getNetworkClient()
+	if err != nil {
+		return err
+	}
+
+	res := monitors.Delete(client, d.Id())
+	return res.Err
+
+}
+
+func resourceLBaaSMonitorRead(d *schema.ResourceData, meta interface{}) error {
+	p := meta.(*Config)
+	client, err := p.getNetworkClient()
+	if err != nil {
+		return err
+	}
+
+	monitor, err := monitors.Get(client, d.Id()).Extract()
+	if err != nil {
+		httpError, ok := err.(*perigee.UnexpectedResponseCodeError)
+		if !ok {
+			return err
+		}
+
+		if httpError.Actual == 404 {
+			d.SetId("")
+			return nil
+		}
+
+		return err
+	}
+
+	readMonitor(monitor, d)
+
+	return nil
+
+}
+
+func resourceLBaaSMonitorUpdate(d *schema.ResourceData, meta interface{}) error {
+	p := meta.(*Config)
+	client, err := p.getNetworkClient()
+	if err != nil {
+		return err
+	}
+
+	opts := monitors.UpdateOpts{
+		Delay:      d.Get("delay").(int),
+		Timeout:    d.Get("timeout").(int),
+		MaxRetries: d.Get("max_retries").(int),
+	}
+
+	expectedCodes := d.Get("expected_codes").(string)
+	if len(expectedCodes) > 0 {
+		opts.ExpectedCodes = expectedCodes
+	}
+
+	httpMethod := d.Get("http_method").(string)
+	if len(httpMethod) > 0 {
+		opts.HTTPMethod = httpMethod
+	}
+
+	urlPath := d.Get("url_path").(string)
+	if len(urlPath) > 0 {
+		opts.URLPath = urlPath
+	}
+
+	if d.HasChange("admin_state_up") {
+		adminStateUp := d.Get("admin_state_up").(bool)
+		opts.AdminStateUp = &adminStateUp
+	}
+
+	monitor, err := monitors.Update(client, d.Id(), opts).Extract()
+	if err != nil {
+		return err
+	}
+
+	readMonitor(monitor, d)
+	return nil
+
+}
+
+func readMonitor(monitor *monitors.Monitor, d *schema.ResourceData) {
+	d.Set("type", monitor.Type)
+	d.Set("delay", monitor.Delay)
+	d.Set("timeout", monitor.Timeout)
+	d.Set("max_retries", monitor.MaxRetries)
+	d.Set("admin_state_up", monitor.AdminStateUp)
+
+	if monitor.Type == monitors.TypeHTTP || monitor.Type == monitors.TypeHTTPS {
+		d.Set("expected_codes", monitor.ExpectedCodes)
+		d.Set("http_method", monitor.HTTPMethod)
+		d.Set("url_path", monitor.URLPath)
+	} else {
+		d.Set("expected_codes", "")
+		d.Set("http_method", "")
+		d.Set("url_path", "")
+	}
+
+}
---
+++ builtin/providers/openstack/resource_openstack_lbaas_monitor_test.go
@@ -0,0 +1,137 @@
+package openstack
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform/helper/resource"
+	"github.com/hashicorp/terraform/terraform"
+	"github.com/racker/perigee"
+	"github.com/rackspace/gophercloud/openstack/networking/v2/extensions/lbaas/monitors"
+)
+
+func TestAccOpenstackLBaaSMonitor(t *testing.T) {
+	var monitorId string
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckOpenstackLBaaSMonitorDestroy,
+		Steps: []resource.TestStep{
+			resource.TestStep{
+				Config: testMonitorConfig,
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckOpenstackLBaaSMonitorExists(
+						"openstack_lbaas_monitor.tf_http_monitor", &monitorId),
+				),
+			},
+			resource.TestStep{
+				Config: testMonitorUpdateConfig,
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckOpenstackLBaaSMonitorExists(
+						"openstack_lbaas_monitor.tf_http_monitor", &monitorId),
+				),
+			},
+			resource.TestStep{
+				Config: testMonitorForceNewConfig,
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckOpenstackLBaaSMonitorExists(
+						"openstack_lbaas_monitor.tf_http_monitor", &monitorId),
+				),
+			},
+		},
+	})
+}
+
+func testAccCheckOpenstackLBaaSMonitorDestroy(s *terraform.State) error {
+	config := testAccProvider.Meta().(*Config)
+
+	for _, rs := range s.RootModule().Resources {
+		if rs.Type != "openstack_lbaas_monitor" {
+			continue
+		}
+
+		networkClient, err := config.getNetworkClient()
+		if err != nil {
+			return err
+		}
+
+		_, err = monitors.Get(networkClient, rs.Primary.ID).Extract()
+		if err == nil {
+			return fmt.Errorf("Monitor (%s) still exists.", rs.Primary.ID)
+		}
+
+		httpError, ok := err.(*perigee.UnexpectedResponseCodeError)
+		if !ok || httpError.Actual != 404 {
+			return httpError
+		}
+	}
+
+	return nil
+}
+
+func testAccCheckOpenstackLBaaSMonitorExists(n string, monitorId *string) resource.TestCheckFunc {
+
+	return func(s *terraform.State) error {
+		rs, ok := s.RootModule().Resources[n]
+		if !ok {
+			return fmt.Errorf("Not found: %s", n)
+		}
+
+		if rs.Primary.ID == "" {
+			return fmt.Errorf("No ID is set")
+		}
+
+		config := testAccProvider.Meta().(*Config)
+		networkClient, err := config.getNetworkClient()
+		if err != nil {
+			return err
+		}
+
+		found, err := monitors.Get(networkClient, rs.Primary.ID).Extract()
+		if err != nil {
+			return err
+		}
+
+		if found.ID != rs.Primary.ID {
+			return fmt.Errorf("Subnet not found")
+		}
+
+		monitorId = &found.ID
+
+		return nil
+	}
+}
+
+const testMonitorConfig = `
+resource "openstack_lbaas_monitor" "tf_http_monitor" {
+    type = "HTTP"
+    delay = 25
+    timeout = 2
+    max_retries = 2
+    expected_codes = "200"
+    url_path = "/"
+    http_method = "GET"
+}
+`
+
+const testMonitorUpdateConfig = `
+resource "openstack_lbaas_monitor" "tf_http_monitor" {
+    type = "HTTP"
+    delay = 10
+    timeout = 2
+    max_retries = 2
+    expected_codes = "204"
+    url_path = "/healthcheck"
+    http_method = "GET"
+}
+`
+
+const testMonitorForceNewConfig = `
+resource "openstack_lbaas_monitor" "tf_http_monitor" {
+    type = "PING"
+    delay = 25
+    timeout = 5
+    max_retries = 3
+}
+`
