70f6ac88f22054472f79c1cbb9663db9bff16f27
Guillaume Giamarchi <guillaume.giamarchi@gmail.com>
2015-02-02 01:19:33 +0100

    Firewall rules support
    * Refactor to use the latest gophercloud
    * Handle rules updates
    * Add acceptance tests

--- builtin/providers/openstack/resource_openstack_firewall_rule.go
+++ builtin/providers/openstack/resource_openstack_firewall_rule.go
@@ -2,17 +2,18 @@ package openstack
 
 import (
 	"log"
+	"time"
 
-	"github.com/haklop/gophercloud-extensions/network"
 	"github.com/hashicorp/terraform/helper/schema"
 	"github.com/racker/perigee"
-	"github.com/rackspace/gophercloud"
+	"github.com/rackspace/gophercloud/openstack/networking/v2/extensions/fwaas/rules"
 )
 
 func resourceFirewallRule() *schema.Resource {
 	return &schema.Resource{
 		Create: resourceFirewallRuleCreate,
 		Read:   resourceFirewallRuleRead,
+		Update: resourceFirewallRuleUpdate,
 		Delete: resourceFirewallRuleDelete,
 
 		Schema: map[string]*schema.Schema{
@@ -53,10 +54,6 @@ func resourceFirewallRule() *schema.Resource {
 				Type:     schema.TypeString,
 				Optional: true,
 			},
-			"shared": &schema.Schema{
-				Type:     schema.TypeBool,
-				Optional: true,
-			},
 			"enabled": &schema.Schema{
 				Type:     schema.TypeBool,
 				Optional: true,
@@ -68,37 +65,52 @@ func resourceFirewallRule() *schema.Resource {
 
 func resourceFirewallRuleCreate(d *schema.ResourceData, meta interface{}) error {
 
-	p := meta.(*Config)
-	networksApi, err := p.getNetworkApi()
+	c := meta.(*Config)
+	networkClient, err := c.getNetworkClient()
 	if err != nil {
 		return err
 	}
 
-	access := p.AccessProvider.(*gophercloud.Access)
+	ipVersion := d.Get("ip_version").(int)
+	enabled := d.Get("enabled").(bool)
 
-	ruleConfiguration := network.NewFirewallRule{
+	ruleConfiguration := rules.CreateOpts{
 		Name:                 d.Get("name").(string),
 		Description:          d.Get("description").(string),
 		Protocol:             d.Get("protocol").(string),
 		Action:               d.Get("action").(string),
-		IpVersion:            d.Get("ip_version").(int),
-		SourceIpAddress:      d.Get("source_ip_address").(string),
-		DestinationIpAddress: d.Get("destination_ip_address").(string),
+		IPVersion:            &ipVersion,
+		SourceIPAddress:      d.Get("source_ip_address").(string),
+		DestinationIPAddress: d.Get("destination_ip_address").(string),
 		SourcePort:           d.Get("source_port").(string),
 		DestinationPort:      d.Get("destination_port").(string),
-		Shared:               d.Get("shared").(bool),
-		Enabled:              d.Get("enabled").(bool),
-		TenantId:             access.Token.Tenant.Id,
+		Enabled:              &enabled,
 	}
 
 	log.Printf("[DEBUG] Create firewall rule: %#v", ruleConfiguration)
 
-	firewall, err := networksApi.CreateFirewallRule(ruleConfiguration)
+	rule, err := rules.Create(networkClient, ruleConfiguration).Extract()
+
 	if err != nil {
 		return err
 	}
 
-	d.SetId(firewall.Id)
+	log.Printf("[DEBUG] Firewall rule with id %s : %#v", rule.ID, rule)
+
+	d.SetId(rule.ID)
+
+	time.Sleep(time.Second * 5)
+
+	d.Set("name", rule.Name)
+	d.Set("description", rule.Description)
+	d.Set("protocol", rule.Protocol)
+	d.Set("action", rule.Action)
+	d.Set("ip_version", rule.IPVersion)
+	d.Set("source_ip_address", rule.SourceIPAddress)
+	d.Set("destination_ip_address", rule.DestinationIPAddress)
+	d.Set("source_port", rule.SourcePort)
+	d.Set("destination_port", rule.DestinationPort)
+	d.Set("enabled", rule.Enabled)
 
 	return nil
 }
@@ -106,40 +118,94 @@ func resourceFirewallRuleCreate(d *schema.ResourceData, meta interface{}) error 
 func resourceFirewallRuleRead(d *schema.ResourceData, meta interface{}) error {
 	log.Printf("[DEBUG] Retrieve information about firewall rule: %s", d.Id())
 
-	p := meta.(*Config)
-	networksApi, err := p.getNetworkApi()
+	c := meta.(*Config)
+	networkClient, err := c.getNetworkClient()
 	if err != nil {
 		return err
 	}
 
-	n, err := networksApi.GetFirewallRule(d.Id())
+	rule, err := rules.Get(networkClient, d.Id()).Extract()
+
 	if err != nil {
 		httpError, ok := err.(*perigee.UnexpectedResponseCodeError)
 		if !ok {
 			return err
 		}
-
 		if httpError.Actual == 404 {
 			d.SetId("")
 			return nil
 		}
-
 		return err
 	}
 
-	d.Set("name", n.Name)
+	d.Set("name", rule.Name)
+	d.Set("description", rule.Description)
+	d.Set("protocol", rule.Protocol)
+	d.Set("action", rule.Action)
+	d.Set("ip_version", rule.IPVersion)
+	d.Set("source_ip_address", rule.SourceIPAddress)
+	d.Set("destination_ip_address", rule.DestinationIPAddress)
+	d.Set("source_port", rule.SourcePort)
+	d.Set("destination_port", rule.DestinationPort)
+	d.Set("enabled", rule.Enabled)
 
 	return nil
+}
+
+func resourceFirewallRuleUpdate(d *schema.ResourceData, meta interface{}) error {
+	c := meta.(*Config)
+	networkClient, err := c.getNetworkClient()
+	if err != nil {
+		return err
+	}
+
+	opts := rules.UpdateOpts{}
+
+	if d.HasChange("name") {
+		opts.Name = d.Get("name").(string)
+	}
+	if d.HasChange("description") {
+		opts.Description = d.Get("description").(string)
+	}
+	if d.HasChange("protocol") {
+		opts.Protocol = d.Get("protocol").(string)
+	}
+	if d.HasChange("action") {
+		opts.Action = d.Get("action").(string)
+	}
+	if d.HasChange("ip_version") {
+		ipVersion := d.Get("ip_version").(int)
+		opts.IPVersion = &ipVersion
+	}
+	if d.HasChange("source_ip_address") {
+		opts.SourceIPAddress = d.Get("source_ip_address").(string)
+	}
+	if d.HasChange("destination_ip_address") {
+		opts.DestinationIPAddress = d.Get("destination_ip_address").(string)
+	}
+	if d.HasChange("source_port") {
+		opts.SourcePort = d.Get("source_port").(string)
+	}
+	if d.HasChange("destination_port") {
+		opts.DestinationPort = d.Get("destination_port").(string)
+	}
+	if d.HasChange("enabled") {
+		enabled := d.Get("enabled").(bool)
+		opts.Enabled = &enabled
+	}
+
+	log.Printf("[DEBUG] Updating firewall rules: %#v", opts)
+
+	return rules.Update(networkClient, d.Id(), opts).Err
 }
 
 func resourceFirewallRuleDelete(d *schema.ResourceData, meta interface{}) error {
 	log.Printf("[DEBUG] Destroy firewall rule: %s", d.Id())
 
-	p := meta.(*Config)
-	networksApi, err := p.getNetworkApi()
+	c := meta.(*Config)
+	networkClient, err := c.getNetworkClient()
 	if err != nil {
 		return err
 	}
-
-	return networksApi.DeleteFirewallRule(d.Id())
+	return rules.Delete(networkClient, d.Id()).Err
 }
